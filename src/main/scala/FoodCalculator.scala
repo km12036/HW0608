f1a5814e83928ca6bc9313cc77fe6f814b1ef143
Kristine M <km12036@inbox.lv>
2020-08-07 14:43:43 +0300

    Homework

---
+++ src/main/scala/FoodCalculator.scala
@@ -0,0 +1,11 @@
+object FoodCalculator extends App {
+
+    val food = "potatoes"
+    val price = 0.37
+    val quantity = 20
+    val total=price*quantity
+
+    println(s"It will cost $total EUR to buy $quantity kilos of $food.")
+
+
+}
