3ae8d1663ff67a052acb4ead331c62032120f0c1
Kristine M <km12036@inbox.lv>
2020-08-11 09:56:16 +0300

    Food calculator with arguments

--- src/main/scala/FoodCalculator.scala
+++ src/main/scala/FoodCalculator.scala
@@ -7,5 +7,12 @@ object FoodCalculator extends App {
 
     println(s"It will cost $total EUR to buy $quantity kilos of $food.")
 
+    val cost=args(0).toFloat*args(1).toInt
+    println(s"The first argument is ${args(0)}")
+    println(s"The second argument is ${args(1)}")
+    println(s"The third argument is ${args(2)}")
+
+    println(s"It will cost $cost EUR to buy ${args(1)} kilos of ${args(2)}.")
+
 
 }
